ef2f366804ca688247070f143c7b2d70b67136d3
AryanV311 <159629630+AryanV311@users.noreply.github.com>
2024-12-11 12:37:08 +0530

    drag and drop

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,9 +1,10 @@
 const input = document.querySelector(".input")
 const add = document.querySelector('.add')
 // const trash = document.querySelectorAll(".trash")
-const list = document.querySelector(".list")
-// console.log(add);
+const list = document.querySelectorAll(".list")
+// console.log(list);
 const taskList = document.querySelector('.task-list')
+const mainCard = document.querySelector(".main-card")
 
 const addTask = () => {
     if(input.value.trim() === ""){
@@ -13,9 +14,12 @@ const addTask = () => {
 
     const div = document.createElement('div')
     div.setAttribute("class","list")
+    div.setAttribute("draggable","true")
+   
     div.innerHTML=`<li>${input.value}</li>
                 <i class="fa-solid fa-trash"></i>`;
 
+    console.log(div);
     taskList.appendChild(div)
     
     input.value =""
@@ -34,3 +38,53 @@ taskList.addEventListener("click", (e) => {
     }
     
 });
+
+
+const taskList2 = document.querySelector(".task-list2");
+const mainCard2 = document.querySelector(".main-card2")
+
+let selected = null;
+
+document.body.addEventListener("dragstart", (e) => {
+    console.log("body", e.target.classList);
+    if (e.target.classList.contains("list")) {
+        selected = e.target;
+    }
+});
+
+// for(let li of list){
+//     // console.log(li)
+//     li.addEventListener('dragstart', function(e){
+//         // console.log(e.target)
+//         selected = e.target
+//         return selected
+//     })
+// }
+
+mainCard2.addEventListener('dragover', function(e){
+    e.preventDefault()
+    console.log(e.preventDefault());
+})
+
+mainCard2.addEventListener('drop',function(e){
+    e.preventDefault();
+    console.log(selected);
+    if(selected){
+        taskList2.appendChild(selected)
+        selected = null;
+    }
+})
+
+mainCard.addEventListener('dragover', function(e){
+    e.preventDefault();
+})
+
+mainCard.addEventListener('drop', function(e){
+    e.preventDefault()
+    if(selected){
+        taskList.appendChild(selected)
+        selected = null;
+    }
+})
+
+console.log(taskList);
